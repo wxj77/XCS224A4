231c478410fb14ed1261e7e4952f6593195040e9
Wei Ji <weiji@Weis-MacBook-Pro.local>
2021-03-20 19:00:05 -0700

    finish A4

---
+++ src-quiz/submission.py
@@ -0,0 +1,139 @@
+def multiple_choice_1():
+  """"""
+  # Multiple Choice Response
+  # Return a python collection with the option(s) that you believe are correct
+  # like this:
+  # `return ['a']`
+  # or
+  # `return ['a', 'd']`
+  response = []
+  ### START CODE HERE ###
+  response = ['a', 'c',]#response = ['b', 'c',]
+  ### END CODE HERE ###
+  return response
+
+def multiple_choice_2():
+  """"""
+  # Multiple Choice Response
+  # Return a python collection with the option(s) that you believe are correct
+  # like this:
+  # `return ['a']`
+  # or
+  # `return ['a', 'd']`
+  response = []
+  ### START CODE HERE ###
+  response = ['a', 'b', 'c',]#response = ['c',]#response = ['a', 'c',]
+  ### END CODE HERE ###
+  return response
+
+def multiple_choice_3a():
+  """"""
+  # Multiple Choice Response
+  # Return a python collection with the option(s) that you believe are correct
+  # like this:
+  # `return ['a']`
+  # or
+  # `return ['a', 'd']`
+  response = []
+  ### START CODE HERE ###
+  response = ['a', 'c',]
+  ### END CODE HERE ###
+  return response
+
+def multiple_choice_3b():
+  """"""
+  # Multiple Choice Response
+  # Return a python collection with the option(s) that you believe are correct
+  # like this:
+  # `return ['a']`
+  # or
+  # `return ['a', 'd']`
+  response = []
+  ### START CODE HERE ###
+  response = ['a', 'b',]
+  ### END CODE HERE ###
+  return response
+
+def multiple_choice_4a():
+  """"""
+  # Multiple Choice Response
+  # Return a python collection with the option(s) that you believe are correct
+  # like this:
+  # `return ['a']`
+  # or
+  # `return ['a', 'd']`
+  response = []
+  ### START CODE HERE ###
+  response = ['c', ]#response = ['a', ]#response = ['b', ]
+  ### END CODE HERE ###
+  return response
+
+def multiple_choice_4b():
+  """"""
+  # Multiple Choice Response
+  # Return a python collection with the option(s) that you believe are correct
+  # like this:
+  # `return ['a']`
+  # or
+  # `return ['a', 'd']`
+  response = []
+  ### START CODE HERE ###
+  response = ['a',]
+  ### END CODE HERE ###
+  return response
+
+def multiple_choice_4c():
+  """"""
+  # Multiple Choice Response
+  # Return a python collection with the option(s) that you believe are correct
+  # like this:
+  # `return ['a']`
+  # or
+  # `return ['a', 'd']`
+  response = []
+  ### START CODE HERE ###
+  response = ['a', 'b',]
+  ### END CODE HERE ###
+  return response
+
+def multiple_choice_5a():
+  """"""
+  # Multiple Choice Response
+  # Return a python collection with the option(s) that you believe are correct
+  # like this:
+  # `return ['a']`
+  # or
+  # `return ['a', 'd']`
+  response = []
+  ### START CODE HERE ###
+  response = ['c',]
+  ### END CODE HERE ###
+  return response
+
+def multiple_choice_5b():
+  """"""
+  # Multiple Choice Response
+  # Return a python collection with the option(s) that you believe are correct
+  # like this:
+  # `return ['a']`
+  # or
+  # `return ['a', 'd']`
+  response = []
+  ### START CODE HERE ###
+  response = ['a', 'b',]
+  ### END CODE HERE ###
+  return response
+
+def multiple_choice_5c():
+  """"""
+  # Multiple Choice Response
+  # Return a python collection with the option(s) that you believe are correct
+  # like this:
+  # `return ['a']`
+  # or
+  # `return ['a', 'd']`
+  response = []
+  ### START CODE HERE ###
+  response = ['a', 'b', 'c', ]
+  ### END CODE HERE ###
+  return response
